6a60920aefe13d6179aa8950710c59122623dacd
Your Name <you@example.com>
2022-02-01 13:35:57 +0100

    nyt commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const h2 = document.querySelector("h2");
+console.log(h2);
+
+const h3 = document.querySelector("h3");
+console.log(h3);
+
+const p1 = document.querySelector("article:first-child p");
+console.log(p1);
+
+const p2 = document.querySelector("article:nth-child(2) p");
+console.log(p2);
+
+const img1 = document.querySelector("article:first-child img");
+console.log(img1);
+
+const img2 = document.querySelector("article:nth-child(2) img");
+console.log(img2);
+
+img1.src = "https://placeimg.com/640/480/arch";
+
+img1.alt = "dette er en alt tekst";
+
+img2.src = "https://placeimg.com/640/480/arch";
+
+img2.alt = "dette er en alt tekst";
+
+h1.textContent = "javatitel";
+
+h2.textContent = "javah2";
+
+h3.textConent = "javah3";
+
+p1.textContent = "Java tekst";
+
+p2.textContent = "Java tekst";
+
+

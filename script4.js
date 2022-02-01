ea87366884e1d8a7918adafb500de1109b528b42
Your Name <you@example.com>
2022-02-01 14:15:12 +0100

    4 commit

--- script4.js
+++ script4.js
@@ -36,3 +36,24 @@ img1.alt = "dette er en alt tekst";
 img2.src = "https://placeimg.com/640/480/arch";
 
 img2.alt = "dette er en alt tekst";
+
+const til_article = document.querySelector(".grid");
+let article3 = document.createElement("article");
+til_article.appendChild(article3);
+
+const h3_01 = document.createElement("h3");
+let h3_tekst = document.createTextNode("Article 3");
+h3_01.appendChild(h3_tekst);
+
+const p_01 = document.createElement("p");
+let p_tekst = document.createTextNode(
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet maxime alias quis earum ab voluptate, expedita harum, corruptidolore iusto officia vero rem quia culpa provident facere. Solutafacilis veritatis fugit? Excepturi debitis qui asperiores earumadipisci at consequuntur, ea maxime quasi? Omnis ea id ipsam aliquamaspernatur harum. Totam."
+);
+p_01.appendChild(p_tekst);
+
+const img_01 = document.createElement("img");
+img_01.src = "https://placeimg.com/640/480/arch";
+
+article3.appendChild(img_01);
+article3.appendChild(h3_01);
+article3.appendChild(p_01);
